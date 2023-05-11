e0d912908e334f03974874a4223a723c5a1d4ffd
gil-son <gilszon@gmail.com>
2023-05-11 17:49:38 -0300

    Products

---
+++ Gil.ApiDotNet7.Domain/Entities/Product.cs
@@ -0,0 +1,37 @@
+using System;
+namespace Gil.ApiDotNet7.Domain.Entities
+{
+    public sealed class Product
+    {
+        
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string CodErp { get; private set; }
+        public decimal Price { get; private set; }
+        public ICollection<Purchase> Purchases { get;  set; }
+        
+        Product(string name, string codErp, decimal price)
+        {
+            Validation(name, codErp, price);
+        }
+
+        public Product(int id, string name, string codErp, decimal price)
+        {
+            DomainValidationException.When(id <= 0, "The id is required!");
+            Id = id;
+            Validation(name, codErp, price);
+        }
+
+        private void Validation(string name, string codErp, decimal price)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "The name can't be null!");
+            DomainValidationException.When(string.IsNullOrEmpty(codErp), "The codErp can't be null!");
+            DomainValidationException.When(price <= 0, "The price is required!");
+
+            Name = name;
+            CodErp = codErp;
+            Price = price;
+        }
+
+    }
+}
